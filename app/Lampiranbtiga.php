a0b137f0764e841f91ae81685b63ab5c3fd95ade
unknown <suhairi81@gmail.com>
2016-02-29 08:51:27 +0800

    20160229 1

---
+++ app/Lampiranbtiga.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lampiranbtiga extends Model
+{
+	protected $table = 'lampiranbtiga';
+	protected $primaryKey = 'id';
+	protected $fillable = [
+		'nisbah1', 'markah1',
+		'harta_semasa', 'tanggungan_semasa', 'nisbah2', 'markah2', 
+		'jumlah_tanggungan', 'jumlah_harta', 'nisbah3', 'markah3'
+		'ppk_id'
+		];
+
+	public function ppk() {
+		return $this->belongsTo('App\Ppk');
+	}
+
+
+}
---
+++ database/migrations/2016_02_29_004140_create_lampiranbtiga_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLampiranbtigaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lampiranbtiga', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('nisbah1', 4, 2)->nullable();
+            $table->tinyInteger('markah1', false, false)->nullable();
+            $table->float('harta_semasa', 9, 2)->nullable();
+            $table->float('tanggungan_semasa', 9, 2)->nullable();
+            $table->float('nisbah2', 4, 2)->nullable();
+            $table->tinyInteger('markah2', false, false)->nullable();
+            $table->float('jumlah_tanggungan', 9, 2);
+            $table->float('jumlah_harta', 9, 2);
+            $table->float('nisbah3', 9, 2)->nullable();
+            $table->tinyInteger('markah3', false, false);
+            $table->tinyInteger('ppk_id', false, false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('lampiranbtiga');
+    }
+}
