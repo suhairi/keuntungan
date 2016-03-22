044b86c7f83f5c2b4351fd6d53882b8ad2b16309
Suhairi <suhairi81@gmail.com>
2016-03-22 18:14:20 +0800

    20160323 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,9 +32,9 @@ class HomeController extends Controller
         *           Email Notification
         ******************************************/
 
-        // Mail::send('mail.mailers', ['user' => '', 'ppk' => Auth::user()->ppk->fullname, 'name' => Auth::user()->ppk->name], function($message) {
-        //     $message->to('suhairi81@gmail.com', 'user')->subject('e-Dividen (test)');            
-        // });
+        Mail::send('mail.mailers', ['user' => '', 'ppk' => Auth::user()->ppk->fullname, 'name' => Auth::user()->ppk->name], function($message) {
+            $message->to('suhairi81@gmail.com', 'user')->subject('e-Dividen (test)');            
+        });
 
         // // Mail::send('mail.mailers', ['user' => '', 'ppk' => Auth::user()->ppk->fullname, 'name' => Auth::user()->ppk->name], function($message) {
         // //     $message->to('rozaidi@mada.gov.my', 'user')->subject('e-Dividen (test)');            
@@ -48,30 +48,30 @@ class HomeController extends Controller
         *           SMS Notification
         ******************************************/
 
-        // $ppk = Auth::user()->ppk->name;
+        $ppk = Auth::user()->ppk->name;
 
-        // $url = "http://login.bulksms.my/websmsapi/ISendSMS.aspx";
-        // $fields_string = null;
+        $url = "http://login.bulksms.my/websmsapi/ISendSMS.aspx";
+        $fields_string = null;
 
-        // $fields = array(
-        //     'username' => 'suhairi',
-        //     'password' => 'harry5367' ,           
-        //     'message' => 'e-Dividen. Tuan, PPK ' . $ppk . ' telah memohon pembahagian dividen. Mohon tindakan tuan. Emel pautan/link telah dihantar kepada tuan. Terima kasih.',
-        //     'mobile' => '601114962017',
-        //     'type' => '1',            
-        // );
+        $fields = array(
+            'username' => 'suhairi',
+            'password' => 'harry5367' ,           
+            'message' => 'e-Dividen. Tuan, PPK ' . $ppk . ' telah memohon pembahagian dividen. Mohon tindakan tuan. Emel pautan/link telah dihantar kepada tuan. Terima kasih.',
+            'mobile' => '601114962017',
+            'type' => '1',            
+        );
 
-        // $fields_string = http_build_query($fields);
+        $fields_string = http_build_query($fields);
 
-        // $ch = curl_init();
+        $ch = curl_init();
 
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_POST, count($fields));
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 
-        // $result = curl_exec($ch);
+        $result = curl_exec($ch);
 
-        // curl_close($ch);
+        curl_close($ch);
 
 
 
